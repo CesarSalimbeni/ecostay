85569ba08505a96f93f6b69fc177d576905c6f71
JP-1907 <juanpablorojasvargas07@gmail.com>
2026-06-13 17:31:50 -0400

    Update calificacion.dart

--- lib/models/calificacion.dart
+++ lib/models/calificacion.dart
@@ -4,6 +4,7 @@ class Calificacion {
   final String comentario;
   final DateTime fecha;
   final String nombreUsuario;
+  final int contadorReportes; 
 
   Calificacion({
     required this.id,
@@ -11,5 +12,6 @@ class Calificacion {
     required this.comentario,
     required this.fecha,
     required this.nombreUsuario,
+    this.contadorReportes = 0, 
   });
 }
